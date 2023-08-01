d5373fd43072c0f92a84afa6eef4519255339f96
Javohir Khamidullaev <97449821+Javohir-dev@users.noreply.github.com>
2023-08-01 14:33:37 +0500

    added lessons

---
+++ PRINT/print.py
@@ -0,0 +1,117 @@
+
+# =============================================================================
+# # Izohlar (Comment)
+# 
+# print(2**5)
+# 
+# # Bu dastur beshni kvadratini aniqlash uchun hizmat qiladi.
+# print(2**5)
+# =============================================================================
+
+
+# =============================================================================
+# # print() funksiyasi yordamida turli xil amallarni \
+#     # konsolga chiqarishimiz mumkin.
+# 
+# # Misol uchun:
+# print('Assalomu alaykum!')
+# # Assalomu alaykum!
+# print("Assalomu alaykum!")
+# # Assalomu alaykum!
+# print("Salom!")
+# # SyntaxError: invalid syntax
+# print(12+13*2)
+# # 38
+# print(20/4)
+# # 5.0
+# print(4**2)
+# # 16
+# =============================================================================
+
+
+# =============================================================================
+# # Agar matnni bir necha qatordan iborat bo'lishi hohlasangiz\
+#     # quyidagi usuldan foydalanishingiz mumkin.
+#     
+# # print("""Assalomu alaykum,
+# # men Javohir Web YouTube
+# # kanali asoschisiman.""")
+# # Assalomu alaykum,
+# # men Javohir Web YouTube
+# # kanali asoschisiman.
+# 
+# # Agar matn bir qatorda yozilib konsonla qatorlarga \
+#     # bo'linib chiqishini istasangiz `\n`dan foydalanishingiz mumkin.
+#     
+# # Misol uchun:
+#     
+# print("Assalomu alaykum,\nMen Javohir Web YouTube kanali asoschisiman.")
+# # Assalomu alaykum,
+# #  Men Javohir Web YouTube kanali asoschisiman.
+# =============================================================================
+
+
+# =============================================================================
+# # Matinlar bilan ishlashda extiyot bo'lishimiz kerak bo'lgan \
+#     # narsalarimiz ichida qo'shtirnoq va birtirnoqlar bor.
+# 
+# # Misol uchun:
+# 
+# print('Men juda "yaxshi" insonman.')
+# # Men juda "yaxshi" insonman.
+# 
+# # Yana bir usuli:
+# print("Men juda \"yaxshi\" insonman.")
+# # Men juda "yaxshi" insonman.
+# 
+# print('Men juda \'yaxshi\' insonman.')
+# # SyntaxError: invalid syntax
+# 
+# print("Hello World")
+# # SyntaxError: EOL while scanning string literal.
+# =============================================================================
+
+
+# =============================================================================
+# # Arifmetik amallar 
+# 
+# print(5+5*2)
+# # 15
+# 
+# print(20/3)
+# # 6.666666666666667
+# 
+# print(20/5)
+# # 4.0
+# 
+# print(20//5)
+# # 4
+# 
+# print(20//3)
+# # 6
+# 
+# print(2**6)
+# # 64
+# 
+# print("To'rtning kvadrati" , 2**4 , "ga teng")
+# # To'rtning kvadrati 16 ga teng
+# =============================================================================
+
+
+# =============================================================================
+# # Amaliyot uchun:
+# 
+# 1.
+# "olma", "behi", "olcha", "o'rik"
+# 
+# 2.
+# 6ning 4-darajasini toping va quyidagidek chiqaring.
+# 6ning 4-darajasi 000ga teng
+# 
+# 3.
+# O'zingiz haqingizdagi ma'lumotni qatorlarga bo'lib yozing.
+# Assalomu alaykum, 
+# mening ismim Javohir.
+# 
+# =============================================================================
+
---
+++ variables/variables.py
@@ -0,0 +1,48 @@
+# O'zgaruvchilar
+
+# =============================================================================
+# var = "var matni"
+# baz = 'baz'
+# num = 3.14
+# 
+# print(var)
+# print(baz)
+# print(num)
+# # var = "New text"
+# print(var)
+# =============================================================================
+
+
+# =============================================================================
+# # O'zgaruvchilarni nomlashda ishlatib bo'lmaydigan kalit so'zlar mavjud.
+# 
+# # O'zgaruvchi nomlashda o'zgaruvchiga saqlayotgan\
+#     # ma'lumotiga bog'liq nom qo'ying
+# 
+# # O'zgaruvchilarni nomlashda pastki chiziq (_),
+# # raqamlar (0-9), faqat lotin harflari (A-Z)-(a-z)
+# 
+# ism = 'Javohir'
+# familiya = 'Khamidullaev'
+# print(ism, familiya)
+# 
+# fullname = ism + " " + familiya
+# print(fullname)
+# =============================================================================
+
+
+# =============================================================================
+# # Amaliyot:
+#     
+# # Ism familiyangiz va tug'ilgan joyingiz haqida quyidagidek ma'lumot bering
+# # >>> Mening ismim Palonchiev Pistonchi va, men palonchi joyda tug'ilganman.
+# 
+# # matn = "Mening ismim" + ism + " " + familiya + "va, men" + joy + "joyda tug'ilganman"
+# 
+# # Ko'paytirish jadvalidagilarni ba'zilarini takrorlang.
+# # Misol uchun
+# 
+# amal = 9 * 6
+# print("9 * 6 =", amal)
+# # 9 * 6 = 54
+# =============================================================================
