da41660e25881751629fc467dc04bd0241df7516
Javohir Khamidullaev <97449821+Javohir-dev@users.noreply.github.com>
2023-09-08 18:16:56 +0500

    Update string.py

--- string/string.py
+++ string/string.py
@@ -4,38 +4,69 @@
 ism = "John"
 familiya = 'Smith'
 yosh = '21'
-info = ism + familiya
+# info = ism + familiya
 
 # print(info)
-# JohnSmith
 
-info = ism + ' ' + familiya
+# =============================================================================
+# # JohnSmith
+# 
+# info = ism + ' ' + familiya
 # print(info)
-# John Smith
-
-message = "Juda kulguli🤣😂😂"
+# # John Smith
+# 
+# message = "Juda kulguli🤣😂😂"
 # print(message)
-# Juda kulguli🤣😂😂
-
-info = "Men" + ' '  + ism + ' ' + familiya + " " + "va, men"  + " " + yosh + " " +  "daman."
-
+# # Juda kulguli🤣😂😂
+# 
+# info = "Men" + ' '  + ism + ' ' + familiya + " " + "va, men"  + " " + yosh + " " +  "daman."
+# 
 # print(info)
-# Men John Smith va, men 21 daman.
-info = f'Men {ism} {familiya} va, men {yosh} daman.'
-
+# # Men John Smith va, men 21 daman.
+# info = f'Men {ism} {familiya} va, men {yosh} daman.'
+# 
 # print(info)
-# Men John Smith va, men 21 daman.
+# # Men John Smith va, men 21 daman.
+# =============================================================================
 
-warning = '1234567890!@#$%^"`&*()_+-=":?>\'<,./{}[]|'
-
+# =============================================================================
+# warning = '1234567890!@#$%^"`&*()_+-=":?>\'<,./{}[]|'
+# 
 # print(f"""{warning}
 # Siz faqatgina shu belgilardan foydalana olasiz!""")
-# 1234567890!@#$%^"&*()_+-=":?>'<,./{}[]|
-# Siz faqatgina shu belgilardan foydalana olasiz!
-
-
+# # 11234567890!@#$%^"`&*()_+-=":?>\'<,./{}[]|
+# # Siz faqatgina shu belgilardan foydalana olasiz!
+# 
+# 
 # print(f"{warning}\nSiz faqatgina shu belgilardan foydalana olasiz!")
-# 1234567890!@#$%^"&*()_+-=":?>'<,./{}[]|
-# Siz faqatgina shu belgilardan foydalana olasiz!
-
+# # 1234567890!@#$%^"`&*()_+-=":?>\'<,./{}[]|
+# # Siz faqatgina shu belgilardan foydalana olasiz!
+# 
 # print(f"{ism}\t{familiya}")
+# =============================================================================
+
+# =============================================================================
+# # STRIP()
+# laptop = '   laptop    '
+# print("Bu", laptop.lstrip(), "meniki.")
+# print("Bu", laptop.rstrip(), "meniki.")
+# print("Bu", laptop.strip(), "meniki.")
+# 
+# =============================================================================
+
+# =============================================================================
+# # TITLE(), UPPER(), LOWER(), CAPITALIZE()
+# 
+# ism = 'abdulaziz QURBONOV'
+# # print(ism.lower())
+# # print(ism.upper())
+# # print(ism.capitalize())
+# print(ism.title())
+# =============================================================================
+
+# INPUT()
+name = input('Iltimos ismingizni kiriting:\n>>> ')
+print(f"Assalomu alaykum, {name.title()}. Nima savolingiz bor?")
+
+
+
